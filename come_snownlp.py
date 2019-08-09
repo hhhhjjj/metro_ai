ed95cba99b9895110d4ded9e0ce01678759deaf7
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-08-09 16:02:46 +0800

    Add files via upload

--- come_snownlp.py
+++ come_snownlp.py
@@ -1,4 +1,5 @@
 from snownlp import SnowNLP
+from snownlp import sentiment
 text = u"这个书包的质量不太好，我很生气"
 s = SnowNLP(text)
 print(",".join(s.words))
@@ -14,5 +15,8 @@ for sentence in s.sentences:
 
 print(s.sentiments)
 # 情感分析，积极的概率
-# 我这都很生气了，积极概率还这么高。。
-# 当然这个是可以通过训练来优化的
+# 我这都很生气了，积极概率还这么高。。,训练之前0.7多，训练之后就0.1了，明显下降了很多
+# 当然情感和分词都是可以通过训练来优化的
+# 每个词有权重的
+# sentiment.train(r"C:\python_code\metro_ai\Lib\site-packages\snownlp\sentiment\negative.txt", r"C:\python_code\metro_ai\Lib\site-packages\snownlp\sentiment\positive.txt")
+# sentiment.save(r"C:\python_code\metro_ai\Lib\site-packages\snownlp\sentiment\sentiment.marshal")
